4d9b43de76317d842ab67c610cd5f9f33b17db66
thaling <thaling@verizon.net>
2022-10-25 10:15:58 -0600

    created models and added dateFormat

---
+++ models/Thought.js
@@ -0,0 +1,71 @@
+const { timeStamp } = require('console');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timeStamp => dateFormat(timeStamp)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false,
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+        
+        username: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        thoughtText: {
+            type: String,
+            required: 'Thought is required',
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timeStamp => dateFormat(timeStamp)
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
+        id: false
+    }
+)
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+
+const Thought = model('Thought', ThoughtSchema)
+
+module.exports = Thought
